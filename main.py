0becec686e97a8cef2b172a9a1e72ef87220cc30
김경민 <kimgyeongmin5348@gmail.com>
2026-09-15 11:50:53 +0900

    배포수정

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
+import uvicorn
 from backend.src.api.app import app
 
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
